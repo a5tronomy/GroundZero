b4e4904e725d3b591aa774431d08c597ae48a1fc
a5tronomy <67793178+a5tronomy@users.noreply.github.com>
2026-05-13 13:01:43 +0200

    Build fix

--- Source/GroundZeroGame.Target.cs
+++ Source/GroundZeroGame.Target.cs
@@ -3,6 +3,11 @@
 public class GroundZeroGameTarget : TargetRules {
 	public GroundZeroGameTarget(TargetInfo Target) : base(Target) {
 		Type = TargetType.Game;
+		
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+        CppStandard = CppStandardVersion.Cpp20;
+
 		ExtraModuleNames.AddRange(new string[] {
 			"GroundZero",
 		});
